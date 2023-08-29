6eb66568a5828365460ef33467515a9b3ddb7f85
DevHumbleChris <dev.humblechris254@gmail.com>
2023-08-29 17:45:01 +0300

    redesigned the UI

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,4 @@
-import  { useState } from 'react';
-import { hamburger } from "../assets/icons";
+import { useState } from "react";
 import { navLinks } from "../constants";
 
 const Nav = () => {
@@ -10,14 +9,14 @@ const Nav = () => {
   };
 
   return (
-    <header className='padding-x py-8 absolute z-10 w-full'>
-      <nav className='flex justify-between items-center max-container'>
-        <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+    <header className="padding-x py-8  w-full">
+      <nav className="flex justify-between items-center max-container">
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
           {navLinks.map((item) => (
             <li key={item.label}>
               <a
                 href={item.href}
-                className='font-montserrat leading-normal text-lg text-slate-gray'
+                className="font-montserrat leading-normal text-lg text-slate-gray"
               >
                 {item.label}
               </a>
@@ -25,23 +24,50 @@ const Nav = () => {
           ))}
         </ul>
 
-        <div className='hidden max-lg:block'>
-          <img
-            src={hamburger}
-            alt='hamburger icon'
-            width={25}
-            height={25}
-            onClick={toggleMobileMenu}
-          />
-        </div>
+        <button
+          className="lg:hidden fixed top-5 right-5 bg-white rounded-md z-50 shadow-zinc-500/50 shadow-2xl border p-2"
+          onClick={toggleMobileMenu}
+        >
+          {isMobileMenuOpen ? (
+            <svg
+              className="w-8 h-auto"
+              xmlns="http://www.w3.org/2000/svg"
+              width="1em"
+              height="1em"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41z"
+              ></path>
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1em"
+              height="1em"
+              viewBox="0 0 16 16"
+              className="w-8 h-auto"
+            >
+              <path
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="1.5"
+                d="M2.75 12.25h10.5m-10.5-4h10.5m-10.5-4h10.5"
+              ></path>
+            </svg>
+          )}
+        </button>
 
         {isMobileMenuOpen && (
-          <ul className='flex flex-col items-center gap-4'>
+          <ul className="fixed z-50 top-[5rem] rounded p-5 right-5 bg-coral-red flex flex-col gap-4 shadow text-white">
             {navLinks.map((item) => (
               <li key={item.label}>
                 <a
                   href={item.href}
-                  className='font-montserrat leading-normal text-lg text-slate-gray'
+                  className="font-montserrat leading-normal text-lg"
                 >
                   {item.label}
                 </a>
